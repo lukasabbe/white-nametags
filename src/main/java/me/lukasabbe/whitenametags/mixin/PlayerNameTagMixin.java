91437ebe8fb94c25de13aff6221040310dc8909d
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2024-08-01 21:16:08 +0200

    init

---
+++ src/main/java/me/lukasabbe/whitenametags/mixin/PlayerNameTagMixin.java
@@ -0,0 +1,18 @@
+package me.lukasabbe.whitenametags.mixin;
+
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.text.Text;
+import org.joml.Matrix4f;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(EntityRenderer.class)
+public class PlayerNameTagMixin {
+    @Redirect(method = "renderLabelIfPresent", at= @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;draw(Lnet/minecraft/text/Text;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/font/TextRenderer$TextLayerType;II)I"))
+    private int injected(TextRenderer instance, Text text, float x, float y, int color, boolean shadow, Matrix4f matrix, VertexConsumerProvider vertexConsumers, TextRenderer.TextLayerType layerType, int backgroundColor, int light){
+        return instance.draw(text,x,y,0xFFFFFFFF,true,matrix,vertexConsumers, TextRenderer.TextLayerType.NORMAL,0x00FFFFFF,light);
+    }
+}
