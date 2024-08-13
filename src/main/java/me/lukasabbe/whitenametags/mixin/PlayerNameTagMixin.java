7986b69e27bf4f26f83ddf510635c3305a1778ee
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2024-08-13 19:23:57 +0200

    to 1.20.4

--- src/main/java/me/lukasabbe/whitenametags/mixin/PlayerNameTagMixin.java
+++ src/main/java/me/lukasabbe/whitenametags/mixin/PlayerNameTagMixin.java
@@ -4,15 +4,15 @@ import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.text.Text;
-import net.minecraft.util.math.Matrix4f;
+import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(EntityRenderer.class)
 public class PlayerNameTagMixin {
-    @Redirect(method = "renderLabelIfPresent", at= @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;draw(Lnet/minecraft/text/Text;FFIZLnet/minecraft/util/math/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;ZII)I"))
-    private int injected(TextRenderer instance, Text text, float x, float y, int color, boolean shadow, Matrix4f matrix, VertexConsumerProvider vertexConsumers, boolean seeThrough, int backgroundColor, int light){
-        return instance.draw(text,x,y,0xffffff,true,matrix,vertexConsumers,false,0x0,light);
+    @Redirect(method = "renderLabelIfPresent", at= @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;draw(Lnet/minecraft/text/Text;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/font/TextRenderer$TextLayerType;II)I"))
+    private int injected(TextRenderer instance, Text text, float x, float y, int color, boolean shadow, Matrix4f matrix, VertexConsumerProvider vertexConsumers, TextRenderer.TextLayerType layerType, int backgroundColor, int light){
+        return instance.draw(text,x,y,0xFFFFFFFF,true,matrix,vertexConsumers, TextRenderer.TextLayerType.NORMAL,0x00FFFFFF,light);
     }
 }
