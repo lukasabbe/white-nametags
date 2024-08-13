c33fbb1490504c8a3f1bc4cee8b41ee664a1ab99
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2024-08-13 19:00:28 +0200

    to 1.16.1

--- src/main/java/me/lukasabbe/whitenametags/mixin/PlayerNameTagMixin.java
+++ src/main/java/me/lukasabbe/whitenametags/mixin/PlayerNameTagMixin.java
@@ -3,16 +3,16 @@ package me.lukasabbe.whitenametags.mixin;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderer;
-import net.minecraft.client.util.math.Matrix4f;
+import net.minecraft.text.StringRenderable;
+import net.minecraft.util.math.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(EntityRenderer.class)
 public class PlayerNameTagMixin {
-    @Redirect(method = "renderLabelIfPresent", at= @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;draw(Ljava/lang/String;FFIZLnet/minecraft/client/util/math/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;ZII)I"))
-    private int injected(TextRenderer instance, String text, float x, float y, int color, boolean shadow, Matrix4f matrix, VertexConsumerProvider vertexConsumerProvider, boolean seeThrough, int backgroundColor, int light){
-        return instance.draw(text,x,y,0xffffff,true,matrix,vertexConsumerProvider,false,0x0,light);
-        //return instance.draw(text,x,y,0xFFFFFFFF,true,matrix,matrix, vertexConsumerProvider ,true,0x00FFFFFF);
+    @Redirect(method = "renderLabelIfPresent", at= @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;draw(Lnet/minecraft/text/StringRenderable;FFIZLnet/minecraft/util/math/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;ZII)I"))
+    private int injected(TextRenderer instance, StringRenderable text, float x, float y, int color, boolean shadow, Matrix4f matrix, VertexConsumerProvider vertexConsumers, boolean seeThrough, int backgroundColor, int light){
+        return instance.draw(text,x,y,0xffffff,true,matrix,vertexConsumers,false,0x0,light);
     }
 }
