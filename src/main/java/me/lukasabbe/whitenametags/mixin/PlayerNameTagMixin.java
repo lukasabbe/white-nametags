02e90a3ebaf99e579ee949e9846048690130c7e8
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2025-09-17 14:08:53 +0200

    Alot of changes

--- src/main/java/me/lukasabbe/whitenametags/mixin/PlayerNameTagMixin.java
+++ src/main/java/me/lukasabbe/whitenametags/mixin/PlayerNameTagMixin.java
@@ -1,18 +1,38 @@
 package me.lukasabbe.whitenametags.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.class_12075;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.command.LabelCommandRenderer;
+import net.minecraft.client.render.command.OrderedRenderCommandQueue;
+import net.minecraft.client.render.command.OrderedRenderCommandQueueImpl;
 import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.render.entity.state.EntityRenderState;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
 import net.minecraft.text.Text;
+import net.minecraft.util.math.Vec3d;
 import org.joml.Matrix4f;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(EntityRenderer.class)
+import java.util.List;
+
+@Mixin(targets = "net/minecraft/client/render/command/LabelCommandRenderer$class_12050")
 public class PlayerNameTagMixin {
-    @Redirect(method = "renderLabelIfPresent", at= @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;draw(Lnet/minecraft/text/Text;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/font/TextRenderer$TextLayerType;II)V"))
-    private void injected(TextRenderer instance, Text text, float x, float y, int color, boolean shadow, Matrix4f matrix, VertexConsumerProvider vertexConsumers, TextRenderer.TextLayerType layerType, int backgroundColor, int light){
-        instance.draw(text,x,y,0xFFFFFFFF,true,matrix,vertexConsumers, TextRenderer.TextLayerType.NORMAL,0x00FFFFFF,light);
+    @Shadow @Final
+    List<OrderedRenderCommandQueueImpl.LabelCommand> field_62988;
+
+    @Inject(method = "method_74829", at= @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z", shift = At.Shift.BEFORE), cancellable = true)
+    public void injected(MatrixStack matrixStack, Vec3d vec3d, Text text, boolean bl, int i, double d, class_12075 arg, CallbackInfo ci, @Local float f, @Local(ordinal = 1) int j, @Local(ordinal = 2) int k, @Local Matrix4f matrix4f){
+        this.field_62988.add(new OrderedRenderCommandQueueImpl.LabelCommand(matrix4f, f, (float)j, text, i, 0xFFFFFFFF, 0, d));
+        matrixStack.pop();
+        ci.cancel();
     }
 }
